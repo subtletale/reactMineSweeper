5d2b4a0b7982f14290e1d325ef62faa49245383d
Connor Bentley <connor.bentley@mintek.com>
2018-11-16 15:58:31 -0500

    styling added for intuitive interaction

--- src/components/grid-square.jsx
+++ src/components/grid-square.jsx
@@ -24,14 +24,25 @@ class GridSquare extends Component {
       }
     }
 
-    let _handleStyling = (square) => {
-
+    let _handleStyling = (square, status) => {
+      let classname = ['subSquare']
+      switch (square) {
+        case ' ': classname.push('unTouched'); break
+        case '_': classname.push('clean'); break
+        case 'F': classname.push('flagged'); break
+        case '*': classname.push((status === 'won' || status === 'lost') ? 'bomb' : ''); break
+        case '@': classname.push('flagged'); break
+        default: classname.push('close'); break
+      }
+      return classname.join(' ')
     }
 
     return (
-        <div className="square" onClick={click}>
+      <div className="square">
+        <div className={_handleStyling(this.props.square, this.props.gameStatus)} onClick={click}>
           {_handleContent(this.props.square, this.props.gameStatus)}
         </div>
+      </div>
     )
   }
 }
